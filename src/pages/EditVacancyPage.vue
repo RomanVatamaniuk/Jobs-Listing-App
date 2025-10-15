5a701e52304a8fc1d386f745fdf695642bfebca6
RomanVatamaniuk <romanvatamaaniuk@gmail.com>
2025-10-15 19:04:44 +0300

    Made fixes for deployment

--- src/pages/EditVacancyPage.vue
+++ src/pages/EditVacancyPage.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="container">
     <h1 class="mb-3">Edit Vacancy</h1>
-    <CreateVacancyForm :job="job"/>
+    <CreateVacancyForm v-if="job" :job="job" />
+    <p v-else>Loading...</p>
   </div>
 </template>
 
@@ -9,12 +10,12 @@
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { doc, getDoc } from 'firebase/firestore'
-import { db } from '@/firebase.ts'
+import { db } from '@/firebase'
 import CreateVacancyForm from '@/components/ui/CreateVacancyForm.vue'
-import type { Job } from '@/interfaces/job.interface.ts'
+import type { Job } from '@/interfaces/job.interface'
 
 const route = useRoute()
-const job = ref<Job | null>(null)
+const job = ref<Job>()
 
 onMounted(async () => {
   const jobId = route.params.id as string
